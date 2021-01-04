816316c7996ee7ebe4deb57ab5ee61952f7ccfca
graph1589 <graph1589@gmail.com>
2021-01-04 13:43:41 +0300

    some comments and logs removed

--- src/init3.js
+++ src/init3.js
@@ -19,7 +19,6 @@ import parseXML from './parser';
 const proxy = 'https://cors-anywhere.herokuapp.com/';
 
 export default () => {
-  // стейт
   const state = {
     form: {
       processState: 'filling',
@@ -34,21 +33,17 @@ export default () => {
     },
   };
 
-  // константы
   const form = document.querySelector('[class="rss-form form-inline"]');
   const urlField = document.querySelector('[class="form-control"]');
   const submitButton = document.querySelector('[class="btn btn-primary"]');
 
   const feedbackSuccess = document.querySelector('[class="feedback text-success"]');
-  // возможно, здесь также понадобится данжер и succes alert
 
-  // стейт хендлер
   const processStateHandler = (processState) => {
     console.log(processState);
     switch (processState) {
       case 'failed':
         submitButton.disabled = false;
-        // TODO render error
         break;
       case 'filling':
         submitButton.disabled = false;
@@ -57,10 +52,7 @@ export default () => {
         submitButton.disabled = true;
         break;
       case 'finished':
-        // state.form.injectedUrl = '';
         urlField.value = '';
-        // здесь, вероятно, вся форма исчезнет при удачном добавлении фида
-        // в таком случае нужно будет переопределить контейнер на фидбек под формой
         break;
       default:
         throw new Error(`Unknown state: ${processState}`);
@@ -84,11 +76,9 @@ export default () => {
         renderInputError(value);
         break;
       case 'form.feedError':
-        console.log('feed error already in watcher');
         renderFeedError(value);
         break;
       case 'layout.posts':
-        console.log('watched state, state.posts changed');
         renderLayout(state);
         break;
       default:
@@ -99,7 +89,6 @@ export default () => {
   const getFeedsList = () => watchedState.layout.feeds.map((feed) => feed.url);
 
   const updateValidationState = () => {
-    // здесь валидация и изменение стейта валидно и ошибки, если есть
     const error = validate(watchedState.form.injectedUrl, getFeedsList());
     watchedState.form.valid = (error === '');
     console.log(`updatevalidationstate-ERROR-${error}`);
@@ -109,14 +98,10 @@ export default () => {
   const addRSS = ({
     url, streamTitle, streamDescription, posts,
   }) => {
-    console.log('adding rss'); // сюда проходит
-    console.log(state);
     watchedState.layout.feeds = [{ url, streamTitle, streamDescription }, ...state.layout.feeds];
     watchedState.layout.posts = posts.concat(state.layout.posts);
-    console.log(state);
   };
 
-  // евент листнер на ввод
   urlField.addEventListener('input', (e) => {
     feedbackSuccess.textContent = '';
     watchedState.form.processState = 'filling';
@@ -124,25 +109,16 @@ export default () => {
     updateValidationState(watchedState);
   });
 
-  // евент листнер на сабмит
-  // const promise = new Promise();
-
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('sending');
     watchedState.form.processState = 'sending';
-    // тут промис с запросом на прокси / или нет?
     axios.get(`${proxy}${urlField.value}`)
       .then((response) => {
-        // console.log(response.request.response);
         const parsedRSS = parseXML(response.request.response, urlField.value);
         console.log(`response.data = ${response.data}`);
         addRSS(parsedRSS);
-        // console.log(parsedRSS);
         watchedState.form.processState = 'finished';
-        // здесь очевидно должен быть рендер;
-        // console.log(parsedRSS);
-        // console.log(parseXML(response.request.response).querySelector('title'));
       })
       .catch(() => {
         console.log('catch in GET');
